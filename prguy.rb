e71478cde5072087d12c7288d1c937027ab7fafd
Karim Hamidou <karim@khamidou.com>
2024-07-07 20:38:54 -0700

    Add first formula

---
+++ prguy.rb
@@ -0,0 +1,9 @@
+cask "prguy" do
+  version "0.5"
+  sha256 "956c0c0ff6a4e4d41349c7900635bda7c134dc7cfd18bc6380229f4956f21de4"
+
+  url "https://github.com/khamidou/prguy/releases/download/#{version}/prguy"
+  name "PRGuy"
+  desc "Watch your PRs like a hawk"
+  homepage "https://github.com/khamidou/prguy"
+end
