bbf44323988ce4a2a73ad99de11e766ddd4b8457
Karim Hamidou <karim@khamidou.com>
2024-07-16 07:01:16 -0700

    works better with the right URL

--- Casks/p/prguy.rb
+++ Casks/p/prguy.rb
@@ -2,7 +2,7 @@ cask "prguy" do
   version "0.7"
   sha256 "74ba1c5136bf2d2a8277b4f1ea7d81e65642a80f466345bc9e0f549ff830c7d0"
 
-  url "https://github.com/tonsky/AnyBar/releases/download/#{version}/prguy.zip"
+  url "https://github.com/khamidou/prguy/releases/download/#{version}/prguy.zip"
   name "PRGuy"
   desc "Watch your PRs like a hawk."
   homepage "https://prguy.app"
