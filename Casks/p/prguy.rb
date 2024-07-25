f815a2bb9c81fab5bcf3d06022e6fa0da6384a0e
Karim Hamidou <karim@khamidou.com>
2024-07-25 07:49:27 -0700

    New release

--- Casks/p/prguy.rb
+++ Casks/p/prguy.rb
@@ -1,6 +1,6 @@
 cask "prguy" do
-  version "0.8"
-  sha256 "d8878f551ff790651778babce512379a5b309f0c9e3032552ff2e6b88dd74a7b"
+  version "0.9"
+  sha256 "1fd369f891628121bf77fd5a124d577c063139ba28d97b8a9544d4a9dfb45173"
 
   url "https://github.com/khamidou/prguy/releases/download/#{version}/prguy.zip"
   name "PRGuy"
